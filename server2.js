193ddf2eac4f4e83adc817528278b8b4601670df
Zaira Cortes <zairacortes89@gmail.com>
2024-08-28 00:24:17 -0600

    Asignación de servidores

--- server2.js
+++ server2.js
@@ -11,11 +11,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para la página principal 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'pantallaPrincipal.html'));
+    res.sendFile(path.join(__dirname, 'public', 'principal.html'));
 });
 
 
-// Iniciar el servidor en el puerto 8080
-app.listen(8080, () => {
-    console.log('Servidor escuchando http://localhost:8080');
+// Iniciar el servidor en el puerto 8081
+app.listen(8081, () => {
+    console.log('Servidor escuchando http://localhost:8081');
 });
